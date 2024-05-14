7743fb1afbd794b5be6396c7a2bc555b683ec644
Natashapillay <tashas.rav>
2024-05-14 15:43:53 +0200

    This is my commit

---
+++ src/main/java/com/mycompany/natasha220223475/Natasha220223475.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.natasha220223475;
+
+/**
+ *
+ * @author USER
+ */
+public class Natasha220223475 {
+
+    public static void main(String[] args) {
+        System.out.println("My name is natasha");
+    }
+}
